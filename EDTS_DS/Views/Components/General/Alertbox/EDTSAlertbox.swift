3fabe23f2efb5b5a1ca0a735f1f5aaa9868461d6
Rizka Ghinna Auliya <rizka.ghinna@sg-edts.com>
2026-05-21 11:08:35 +0700

    feat: alertbox properties

--- EDTS_DS/Views/Components/General/Alertbox/EDTSAlertbox.swift
+++ EDTS_DS/Views/Components/General/Alertbox/EDTSAlertbox.swift
@@ -15,9 +15,15 @@ public enum EDTSAlertboxState: String {
     case info = "info"
 }
 
+public enum EDTSAlertboxStyle: String {
+    case `default` = "default"
+    case ribbon = "ribbon"
+}
+
 class EDTSAlertbox: UIView {
     
     @IBOutlet var containerView: UIView!
+    @IBOutlet weak var vContainer: UIView!
     @IBOutlet weak var ivIcon: UIImageView!
     @IBOutlet weak var ivClose: UIImageView!
     @IBOutlet weak var lblTitle: UILabel!
@@ -27,9 +33,15 @@ class EDTSAlertbox: UIView {
     @IBOutlet weak var btnActionHeightConstant: NSLayoutConstraint!
     
     @IBInspectable public var state: String = EDTSAlertboxState.default.rawValue {
-            didSet {
-                setupState()
-            }
+        didSet {
+            setupState()
+        }
+    }
+    
+    @IBInspectable public var style: String = EDTSAlertboxStyle.default.rawValue {
+        didSet {
+            setupStyle()
+        }
     }
     
     @IBInspectable public var label: String? {
@@ -50,15 +62,15 @@ class EDTSAlertbox: UIView {
         }
     }
     
-    @IBInspectable public var iconColor: UIColor? {
+    @IBInspectable public var iconTintColor: UIColor? {
         didSet {
-            ivIcon.tintColor = iconColor
+            ivIcon.tintColor = iconTintColor
         }
     }
     
-    @IBInspectable public var btnCloseColor: UIColor? {
+    @IBInspectable public var btnCloseTintColor: UIColor? {
         didSet {
-            ivClose.tintColor = btnCloseColor
+            ivClose.tintColor = btnCloseTintColor
         }
     }
     
@@ -73,6 +85,16 @@ class EDTSAlertbox: UIView {
             setupButtonVisibility()
         }
     }
+    
+    private var currentState: EDTSAlertboxState {
+        EDTSAlertboxState(rawValue: state) ?? .default
+    }
+    
+    private var alertIcon: String = "ic_information"
+    private var alertTextColor: UIColor? = EDTSColor.grey60
+    private var alertIconColor: UIColor? = EDTSColor.grey50
+    private var alertBgColor: UIColor? = EDTSColor.grey10
+    private var alertBorderColor: UIColor = EDTSColor.grey30
     
     public func configureButton(_ instance: (EDTSButton) -> Void) {
         guard btnAction != nil else { return }
@@ -95,11 +117,13 @@ class EDTSAlertbox: UIView {
 
     override public init(frame: CGRect) {
         super.init(frame: frame)
+        
         setupNib()
     }
 
     required public init?(coder: NSCoder) {
         super.init(coder: coder)
+        
         setupNib()
     }
 
@@ -120,14 +144,110 @@ class EDTSAlertbox: UIView {
     }
     
     private func setupUI() {
-        
+        setupAlertbox()
     }
     
     private func setupState() {
+        setupTextColor()
+        setupIcon()
+        setupIconColor()
+        setupBackgroundColor()
+        setupBorderColor()
+        
+        setupAlertbox()
+    }
+    
+    private func setupStyle() {
         
     }
     
+    // MARK: - State Helpers
+
+    private func themed<T>(`default`: T, info: T, success: T, error: T, warning: T) -> T {
+        switch currentState {
+        case .default: return `default`
+        case .info: return info
+        case .success: return success
+        case .error: return error
+        case .warning: return warning
+        }
+    }
+    
+    private func setupAlertbox() {
+        vContainer.layer.cornerRadius = 8
+        vContainer.layer.borderWidth = 1
+        vContainer.layer.borderColor = alertBorderColor.cgColor
+        vContainer.backgroundColor = alertBgColor
+        lblTitle.textColor = alertTextColor
+        ivIcon.tintColor = alertIconColor
+    }
+    
+    private func setupTextColor() {
+        if EDTSColor.theme == EDTSColorTheme.klikIDM {
+            alertTextColor = EDTSColor.grey80
+        } else {
+            alertTextColor = themed(
+                default: EDTSColor.grey50,
+                info: EDTSColor.blue50,
+                success: EDTSColor.successStrong,
+                error: EDTSColor.errorStrong,
+                warning: EDTSColor.warningStrong
+            )
+        }
+    }
+    
+    private func setupIcon() {
+        if EDTSColor.theme == EDTSColorTheme.klikIDM {
+            alertIcon = themed(
+                default: "ic_information",
+                info: "ic_information",
+                success: "ic_success",
+                error: "ic_error",
+                warning: "ic_attention"
+            )
+        } else {
+            alertIcon = themed(
+                default: "ic_information",
+                info: "ic_information",
+                success: "ic_success",
+                error: "ic_attention",
+                warning: "ic_notice"
+            )
+        }
+    }
+    
+    private func setupIconColor() {
+        alertIconColor = themed(
+            default: EDTSColor.grey50,
+            info: EDTSColor.blue50,
+            success: EDTSColor.successStrong,
+            error: EDTSColor.errorStrong,
+            warning: EDTSColor.warningStrong
+        )
+    }
+    
+    private func setupBackgroundColor() {
+        alertBgColor = themed(
+            default: EDTSColor.grey10,
+            info: EDTSColor.primaryWeak,
+            success: EDTSColor.successWeak,
+            error: EDTSColor.errorWeak,
+            warning: EDTSColor.warningWeak
+        )
+    }
+    
+    private func setupBorderColor() {
+        alertBgColor = themed(
+            default: EDTSColor.grey30,
+            info: EDTSColor.primaryStrong,
+            success: EDTSColor.successStrong,
+            error: EDTSColor.errorStrong,
+            warning: EDTSColor.warningStrong
+        )
+    }
+    
     private func setupButtonVisibility() {
+        btnAction.isHidden = isButtonHide
         btnActionTopConstraint?.constant = isButtonHide ? 0 : 8
         btnActionHeightConstant?.constant = isButtonHide ? 0 : btnAction.intrinsicContentSize.height
     }
