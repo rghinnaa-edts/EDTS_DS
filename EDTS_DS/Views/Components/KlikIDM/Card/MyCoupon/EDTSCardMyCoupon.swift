e45829ab4e4282244f5929013e7495626503dd56
Rizka Ghinna Auliya <rizka.ghinna@sg-edts.com>
2026-07-10 11:07:35 +0700

    fix: move card my coupon files, add default value

---
+++ EDTS_DS/Views/Components/KlikIDM/Card/MyCoupon/EDTSCardMyCoupon.swift
@@ -0,0 +1,399 @@
+//
+//  CardMyCoupon.swift
+//  KlikIDM_DS
+//
+//  Created by Yovita Handayiani on 22/12/25.
+//
+
+import UIKit
+
+@IBDesignable
+public class EDTSCardMyCoupon: UIView {
+    // MARK: - Outlets
+    @IBOutlet var containerView: UIView!
+    @IBOutlet weak var ivLeadingIcon: UIImageView!
+    @IBOutlet weak var ivLeadingIconBG: UIView!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var cvBadge: EDTSBadge!
+    @IBOutlet weak var lblDesc: UILabel!
+    @IBOutlet weak var ivTrailingIcon: UIImageView!
+    
+    // MARK: - Inspectables
+    @IBInspectable public var title: String?{
+        didSet{
+            lblTitle.attributedText = nil
+            lblTitle.text = title
+        }
+    }
+    
+    public var titleAttributed: NSAttributedString? {
+        didSet {
+            lblTitle.text = nil
+            lblTitle.attributedText = titleAttributed
+        }
+    }
+    
+    @IBInspectable public var titleFontName: String = "" {
+        didSet {
+            setupTitleFont()
+        }
+    }
+    
+    @IBInspectable public var titleFontSize: CGFloat = CGFloat.zero {
+        didSet {
+            setupTitleFont()
+        }
+    }
+    
+    @IBInspectable public var titleFontWeight: String = "" {
+        didSet {
+            setupTitleFont()
+        }
+    }
+    
+    @IBInspectable public var titleColor: UIColor?{
+        didSet{
+            lblTitle.textColor = titleColor
+        }
+    }
+    
+    @IBInspectable public var desc: String?{
+        didSet{
+            lblDesc.attributedText = nil
+            lblDesc.text = desc
+        }
+    }
+    
+    public var descAttributed: NSAttributedString? {
+        didSet {
+            lblDesc.text = nil
+            lblDesc.attributedText = descAttributed
+        }
+    }
+    
+    @IBInspectable public var descFontName: String = "" {
+        didSet {
+            setupDescFont()
+        }
+    }
+    
+    @IBInspectable public var descFontSize: CGFloat = CGFloat.zero {
+        didSet {
+            setupDescFont()
+        }
+    }
+    
+    @IBInspectable public var descFontWeight: String = "" {
+        didSet {
+            setupDescFont()
+        }
+    }
+    
+    @IBInspectable public var descColor: UIColor?{
+        didSet{
+            lblDesc.textColor = descColor
+        }
+    }
+    
+    @IBInspectable public var isLiquidGlassBg: Bool = true {
+        didSet {
+            setupBackground()
+        }
+    }
+    
+    @IBInspectable public var bgColor: UIColor?{
+        didSet {
+            setupBackground()
+        }
+    }
+    
+    @IBInspectable public var iconLeading: UIImage?{
+        didSet{
+            ivLeadingIcon.image = iconLeading
+        }
+    }
+    
+    @IBInspectable public var iconTintColorLeading: UIColor?{
+        didSet{
+            ivLeadingIcon.image = ivLeadingIcon.image?.withRenderingMode(.alwaysTemplate)
+            ivLeadingIcon.tintColor = iconTintColorLeading
+        }
+    }
+    
+    @IBInspectable public var iconBgColorLeading: UIColor?{
+        didSet {
+            setupLeadingIconBg(iconBgColorLeading)
+        }
+    }
+    
+    @IBInspectable public var iconTrailing: UIImage?{
+        didSet{
+            ivTrailingIcon.image = iconTrailing
+        }
+    }
+    
+    @IBInspectable public var iconTintColorTrailing: UIColor?{
+        didSet{
+            ivTrailingIcon.image = ivTrailingIcon.image?.withRenderingMode(.alwaysTemplate)
+            ivTrailingIcon.tintColor = iconTintColorTrailing
+        }
+    }
+    
+    @IBInspectable public var cornerRadius: CGFloat = CGFloat.zero {
+        didSet {
+            containerView.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    // MARK: - Public Variable
+    public weak var delegate: EDTSCardMyCouponDelegate?
+    
+    // MARK: - Private Variable
+    private let gradientLayer = CAGradientLayer()
+    
+    // MARK: - Initializers
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        setupNib()
+    }
+    
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupNib()
+    }
+    
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = 8
+        containerView.clipsToBounds = false
+        ivLeadingIconBG.applyCircular()
+        ivLeadingIconBG.clipsToBounds = true
+    }
+    
+    // MARK: - Public Function
+    public func configureBadge(_ instance: (EDTSBadge) -> Void) {
+        guard cvBadge != nil else { return }
+        cvBadge.isHidden = false
+        instance(cvBadge)
+    }
+    
+    // MARK: - Setup & Styling
+    private func setupNib() {
+        let bundle = Bundle(for: type(of: self))
+        if let nib = bundle.loadNibNamed("EDTSCardMyCoupon", owner: self, options: nil),
+           let view = nib.first as? UIView {
+            
+            containerView = view
+            addSubview(containerView)
+            containerView.frame = bounds
+            containerView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        }
+        setupUI()
+    }
+    
+    private func setupUI(){
+        containerView.isUserInteractionEnabled = true
+        isUserInteractionEnabled = true
+        containerView.layer.cornerRadius = 8
+        containerView.clipsToBounds = false
+        
+        setupTitleFont()
+        setupDescFont()
+        setupDefaultState()
+        setupBackground()
+        setupPressGesture()
+    }
+    
+    private func setupFontWeight(from value: String) -> UIFont.Weight {
+        let normalized = value
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+            .lowercased()
+        
+        let weight = FontWeight(rawValue: normalized) ?? .semibold
+        
+        switch weight {
+        case .ultralight: return .ultraLight
+        case .thin:       return .thin
+        case .light:      return .light
+        case .regular:    return .regular
+        case .medium:     return .medium
+        case .semibold:   return .semibold
+        case .bold:       return .bold
+        case .heavy:      return .heavy
+        case .black:      return .black
+        }
+    }
+    
+    private func setupTitleFont() {
+        let size = titleFontSize
+        let weight = setupFontWeight(from: titleFontWeight)
+        
+        if !titleFontName.isEmpty {
+            lblTitle.font = UIFont(name: titleFontName, size: size)
+            ?? UIFont.systemFont(ofSize: size, weight: weight)
+        } else {
+            lblTitle.font = UIFont.systemFont(ofSize: size, weight: weight)
+        }
+    }
+    
+    private func setupDescFont() {
+        let size = descFontSize
+        let weight = setupFontWeight(from: descFontWeight)
+        
+        if !descFontName.isEmpty {
+            lblDesc.font = UIFont(name: descFontName, size: size)
+            ?? UIFont.systemFont(ofSize: size, weight: weight)
+        } else {
+            lblDesc.font = UIFont.systemFont(ofSize: size, weight: weight)
+        }
+    }
+    
+    private func setupBackground() {
+        containerView.subviews
+            .filter { $0 is UIVisualEffectView || $0 is LiquidGlassBackgroundView }
+            .forEach { $0.removeFromSuperview() }
+        
+        if isLiquidGlassBg {
+            containerView.backgroundColor = .clear
+            setupLiquidGlass()
+        } else {
+            containerView.backgroundColor = bgColor ?? .clear
+        }
+    }
+    
+    private func setupLiquidGlass(){
+        if #available(iOS 26.0, *) {
+            let glassContainerEffect = UIGlassContainerEffect()
+            glassContainerEffect.spacing = 16
+            
+            let glassContainerView = UIVisualEffectView(effect: glassContainerEffect)
+            glassContainerView.translatesAutoresizingMaskIntoConstraints = false
+            glassContainerView.backgroundColor = .clear
+            glassContainerView.layer.cornerRadius = 8
+            
+            let glassEffect = UIGlassEffect(style: .clear)
+            let glassView = UIVisualEffectView(effect: glassEffect)
+            glassView.translatesAutoresizingMaskIntoConstraints = false
+            glassView.layer.cornerRadius = 8
+            containerView.insertSubview(glassContainerView, at: 0)
+            glassContainerView.contentView.addSubview(glassView)
+            
+            NSLayoutConstraint.activate([
+                glassContainerView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+                glassContainerView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+                glassContainerView.topAnchor.constraint(equalTo: containerView.topAnchor),
+                glassContainerView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+                
+                glassView.leadingAnchor.constraint(equalTo: glassContainerView.contentView.leadingAnchor),
+                glassView.trailingAnchor.constraint(equalTo: glassContainerView.contentView.trailingAnchor),
+                glassView.topAnchor.constraint(equalTo: glassContainerView.contentView.topAnchor),
+                glassView.bottomAnchor.constraint(equalTo: glassContainerView.contentView.bottomAnchor)
+            ])
+        } else {
+            let glassBackground = LiquidGlassBackgroundView()
+            glassBackground.translatesAutoresizingMaskIntoConstraints = false
+            containerView.insertSubview(glassBackground, at: 0)
+            
+            NSLayoutConstraint.activate([
+                glassBackground.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+                glassBackground.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+                glassBackground.topAnchor.constraint(equalTo: containerView.topAnchor),
+                glassBackground.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
+            ])
+        }
+    }
+    
+    private func setupLeadingIconBg(_ BGColor : UIColor?){
+        gradientLayer.type = .radial
+        gradientLayer.colors = [
+            BGColor?.withAlphaComponent(0.5).cgColor ?? EDTSColor.blueDefault.withAlphaComponent(0.5).cgColor,
+            BGColor?.withAlphaComponent(1.0).cgColor ?? EDTSColor.blueDefault.withAlphaComponent(1.0).cgColor
+        ]
+        
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.frame = ivLeadingIconBG.bounds
+        gradientLayer.opacity = 0.3
+        
+        ivLeadingIconBG.backgroundColor = .clear
+        ivLeadingIconBG.layer.insertSublayer(gradientLayer, at: 0)
+        ivLeadingIconBG.frame = ivLeadingIconBG.bounds
+    }
+    
+    private func setupBadge(){
+        cvBadge.labelColor = EDTSColor.white
+        cvBadge.bgColor = EDTSColor.red30
+        cvBadge.cornerRadius = cvBadge.bounds.height / 2
+        cvBadge.borderColor = EDTSColor.white
+        cvBadge.borderWidth = 1
+        cvBadge.label = "0"
+    }
+    
+    private func setupDefaultState(){
+        titleColor = EDTSColor.white
+        lblTitle.font = EDTSFont.B2.Semibold.font
+        lblDesc.font = EDTSFont.B3.Regular.font
+        descColor = EDTSColor.grey30
+        let bundleLeading = Bundle(for: type(of: self))
+        ivLeadingIcon.image = UIImage(named: "img_coupon", in: bundleLeading, compatibleWith: nil)
+        setupLeadingIconBg(EDTSColor.white)
+        let bundleTrailing = Bundle(for: type(of: self))
+        ivTrailingIcon.image = UIImage(named: "ic_chevron_right", in: bundleTrailing, compatibleWith: nil)?.withRenderingMode(.alwaysTemplate)
+        iconTintColorTrailing = EDTSColor.white
+        setupBadge()
+    }
+    
+    // MARK: - Animation
+    private func animateScaleDown() {
+        UIView.animate(
+            withDuration: 0.1,
+            delay: 0,
+            options: [.curveEaseInOut],
+            animations: {
+                self.transform = CGAffineTransform(scaleX: 0.97, y: 0.97)
+            }
+        )
+    }
+    
+    private func animateScaleUp() {
+        UIView.animate(
+            withDuration: 0.1,
+            delay: 0,
+            options: [.curveEaseInOut],
+            animations: {
+                self.transform = .identity
+            }
+        )
+    }
+
+    private func setupPressGesture() {
+        let pressGesture = UILongPressGestureRecognizer(
+            target: self,
+            action: #selector(handlePress(_:))
+        )
+        pressGesture.minimumPressDuration = 0
+        pressGesture.cancelsTouchesInView = false
+        addGestureRecognizer(pressGesture)
+    }
+    
+    @objc private func handlePress(_ gesture: UILongPressGestureRecognizer) {
+        guard !cvBadge.isSkeleton else { return }
+        
+        switch gesture.state {
+        case .began:
+            animateScaleDown()
+        case .ended, .cancelled, .failed:
+            animateScaleUp()
+            delegate?.didSelectCard(self)
+        default:
+            break
+        }
+    }
+}
+
+
+@MainActor
+public protocol EDTSCardMyCouponDelegate: AnyObject {
+    func didSelectCard(_ card: EDTSCardMyCoupon)
+}
---
+++ EDTS_DS/Views/Components/KlikIDM/Card/MyCoupon/EDTSCardMyCouponCell.swift
@@ -0,0 +1,145 @@
+//
+//  CardMyCouponList.swift
+//  KlikIDM_DS
+//
+//  Created by Rizka Ghinna Auliya on 21/01/26.
+//
+
+import UIKit
+
+public class EDTSCardMyCouponCell: UICollectionViewCell {
+    
+    @IBOutlet var containerView: UIView!
+    @IBOutlet weak var containerBackground: UIView!
+    @IBOutlet weak var ivCoupon: UIImageView!
+    @IBOutlet weak var vDivider: DashedLineView!
+    @IBOutlet weak var vSkeletonTop: UIView!
+    @IBOutlet weak var vSkeletonBottom: UIView!
+    
+    public var isSkeleton: Bool = false {
+        didSet {
+            setupSkeleton()
+        }
+    }
+    
+    private var ribbonView: EDTSRibbon?
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupNib()
+    }
+    
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setupNib()
+    }
+    
+    private func setupNib() {
+        let bundle = Bundle(for: type(of: self))
+        if let nib = bundle.loadNibNamed("EDTSCardMyCouponCell", owner: self, options: nil),
+           let view = nib.first as? UIView {
+            containerView = view
+            containerView.frame = bounds
+            containerView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+            addSubview(containerView)
+            
+            setupUI()
+        }
+    }
+    
+    private func setupUI() {
+        DispatchQueue.main.async {
+            self.setupCouponShape()
+        }
+        ivCoupon.applyCircular()
+    }
+    
+    private func setupCouponShape() {
+        let dashedLineY = vDivider.frame.origin.y
+        
+        containerBackground.clipsToBounds = true
+        containerBackground.applyCouponBackground(
+            notchRadius: 4,
+            notchPosition: dashedLineY,
+            cornerRadius: 8
+        )
+        
+        let couponPath = containerView.createCouponPath(
+            in: containerBackground.bounds,
+            notchRadius: 4,
+            notchPosition: dashedLineY,
+            cornerRadius: 8
+        )
+        
+        containerView.backgroundColor = .clear
+        containerView.layer.shadowColor = UIColor.lightGray.cgColor
+        containerView.layer.shadowOpacity = 0.4
+        containerView.layer.shadowOffset = CGSize(width: 0, height: 1)
+        containerView.layer.shadowRadius = 3
+        containerView.layer.masksToBounds = false
+        containerView.layer.shadowPath = couponPath.cgPath
+    }
+    
+    private func ribbonNewUser() {
+        ribbonView?.removeFromSuperview()
+        
+        let newRibbonView = EDTSRibbon()
+        
+        newRibbonView.ribbonText = "Pengguna Baru"
+        newRibbonView.triangleColor = EDTSColor.orange50
+        newRibbonView.containerStartColor = EDTSColor.yellow30
+        newRibbonView.containerEndColor = EDTSColor.orange30
+        newRibbonView.textColor = EDTSColor.white
+        newRibbonView.gravity = .end
+
+        newRibbonView.anchorToView(
+            rootParent: containerView,
+            targetView: containerView,
+            offsetX: 0,
+            offsetY: 4
+        )
+        
+        ribbonView = newRibbonView
+    }
+    
+    private func ribbonCoupon() {
+        ribbonView?.removeFromSuperview()
+        
+        let newRibbonView = EDTSRibbon()
+        
+        newRibbonView.ribbonText = "i-Kupon"
+        newRibbonView.triangleColor = EDTSColor.blue70
+        newRibbonView.containerStartColor = EDTSColor.blue50
+        newRibbonView.containerEndColor = EDTSColor.blue50
+        newRibbonView.textColor = EDTSColor.white
+        newRibbonView.gravity = .end
+
+        newRibbonView.anchorToView(
+            rootParent: containerView,
+            targetView: containerView,
+            offsetX: 0,
+            offsetY: -3,
+        )
+        
+        ribbonView = newRibbonView
+    }
+    
+    private func setupSkeleton() {
+        if isSkeleton {
+            vSkeletonTop.isHidden = false
+            vSkeletonBottom.isHidden = false
+            
+            ribbonView?.removeFromSuperview()
+            ribbonView = nil
+        } else {
+            vSkeletonTop.isHidden = true
+            vSkeletonBottom.isHidden = true
+            
+            DispatchQueue.main.async {
+                self.ribbonCoupon()
+            }
+        }
+    }
+}
