b61c5e5221bf37879d45cf56736d1674b50a3202
yovitaHandayiani <116541773+yovitaHandayiani@users.noreply.github.com>
2026-08-06 09:08:52 +0700

    Revert "fix: chip theming"
    This reverts commit 88f99fc21b28e8bb9f0db5fa4ba4fca7d73e9318.

--- EDTS_DS/Views/Components/General/Chip/EDTSChip.swift
+++ EDTS_DS/Views/Components/General/Chip/EDTSChip.swift
@@ -365,8 +365,6 @@ public class EDTSChip: UIView {
             paddingLeading = 8
             paddingTrailing = 8
         }
-        
-        animateChip(isActive ? .active : .inactive, animated: false)
     }
     
     private func setupIcon() {
